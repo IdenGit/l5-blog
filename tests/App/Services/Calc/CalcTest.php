3308b4289af89cbd9f96a6dead7cc859f6eaada6
den <den@wvst.info>
2015-05-05 10:24:39 +0600

    Full Calc class test cover // TODO cover for Adder, Substractor eth.

--- tests/App/Services/Calc/CalcTest.php
+++ tests/App/Services/Calc/CalcTest.php
@@ -57,21 +57,23 @@ class CalcTest extends TestCase {
     /**
      * @test
      */
-    public function notAdds(){
-
-    }
-
-    /**
-     * @test
-     */
     public function substracts(){
+        $adder = $this->adder;
+        $substractor = $this->substractor->shouldReceive('handle')->once()->andReturn(5)->mock();
+        $multiplicator = $this->multiplicator;
+        $divider = $this->divider;
+        $exponentater = $this->exponentater;
+        $nth_root = $this->nth_root;
+        $modder = $this->modder;
 
-    }
+        $calc = new Calc($adder,$substractor,$multiplicator,$divider,$exponentater,$nth_root,$modder);
+        $a = 10;
+        $b = 5;
 
-    /**
-     * @test
-     */
-    public function notSubstracts(){
+        $rezult = $calc->substract($a,$b);
+
+
+        $this->assertEquals(5,$rezult);
 
     }
 
@@ -79,69 +81,111 @@ class CalcTest extends TestCase {
      * @test
      */
     public function divides(){
+        $adder = $this->adder;
+        $substractor = $this->substractor;
+        $multiplicator = $this->multiplicator;
+        $divider = $this->divider->shouldReceive('handle')->once()->andReturn(5)->mock();
+        $exponentater = $this->exponentater;
+        $nth_root = $this->nth_root;
+        $modder = $this->modder;
 
-    }
+        $calc = new Calc($adder,$substractor,$multiplicator,$divider,$exponentater,$nth_root,$modder);
+        $a = 10;
+        $b = 2;
 
-    /**
-     * @test
-     */
-    public function noDivides(){
+        $rezult = $calc->divide($a,$b);
 
+
+        $this->assertEquals(5,$rezult);
     }
 
     /**
      * @test
      */
     public function multiplies(){
+        $adder = $this->adder;
+        $substractor = $this->substractor;
+        $multiplicator = $this->multiplicator->shouldReceive('handle')->once()->andReturn(25)->mock();
+        $divider = $this->divider;
+        $exponentater = $this->exponentater;
+        $nth_root = $this->nth_root;
+        $modder = $this->modder;
 
+        $calc = new Calc($adder,$substractor,$multiplicator,$divider,$exponentater,$nth_root,$modder);
+        $a = 5;
+        $b = 5;
+
+        $rezult = $calc->multiply($a,$b);
+
+
+        $this->assertEquals(25,$rezult);
     }
 
-    /**
-     * @test
-     */
-    public function unMultiplies(){
-
-    }
-
-    /**
+     /**
      * @test
      */
     public function exponentates(){
+        $adder = $this->adder;
+        $substractor = $this->substractor;
+        $multiplicator = $this->multiplicator;
+        $divider = $this->divider;
+        $exponentater = $this->exponentater->shouldReceive('handle')->once()->andReturn(8)->mock();
+        $nth_root = $this->nth_root;
+        $modder = $this->modder;
 
+        $calc = new Calc($adder,$substractor,$multiplicator,$divider,$exponentater,$nth_root,$modder);
+        $a = 2;
+        $b = 3;
+
+        $rezult = $calc->exponentate($a,$b);
+
+
+        $this->assertEquals(8,$rezult);
     }
 
-    /**
-     * @test
-     */
-    public function unExponentates(){
-
-    }
 
     /**
      * @test
      */
     public function modes(){
+        $adder = $this->adder;
+        $substractor = $this->substractor;
+        $multiplicator = $this->multiplicator;
+        $divider = $this->divider;
+        $exponentater = $this->exponentater;
+        $nth_root = $this->nth_root;
+        $modder = $this->modder->shouldReceive('handle')->once()->andReturn(1)->mock();
 
-    }
+        $calc = new Calc($adder,$substractor,$multiplicator,$divider,$exponentater,$nth_root,$modder);
+        $a = 5;
+        $b = 2;
 
-    /**
-     * @test
-     */
-    public function unModes(){
+        $rezult = $calc->mod($a,$b);
 
+
+        $this->assertEquals(1,$rezult);
     }
 
     /**
      * @test
      */
     public function substractRoots(){
+        $adder = $this->adder;
+        $substractor = $this->substractor;
+        $multiplicator = $this->multiplicator;
+        $divider = $this->divider;
+        $exponentater = $this->exponentater;
+        $nth_root = $this->nth_root->shouldReceive('handle')->once()->andReturn(2)->mock();
+        $modder = $this->modder;
 
+        $calc = new Calc($adder,$substractor,$multiplicator,$divider,$exponentater,$nth_root,$modder);
+        $a = 8;
+        $b = 3;
+
+        $rezult = $calc->nth_root($a,$b);
+
+
+        $this->assertEquals(2,$rezult);
     }
 
-    /**
-     * @test
-     */
-    public function unSubstractRoots(){
-
-    }
 }
